0845260a469d2f1748f6e0b6e68aec347d949b30
linkcoding <kenny.lin.91@gmail.com>
2017-07-20 08:46:53 -0400

    add collections methods

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+require "pry"
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|word| word.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.map do |element|
+    element[2] = "$"
+    element
+  end
+end
+
+def find_a(array)
+  array.select do |element|
+    element.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.reduce(:+)
+end
+
+def add_s(array)
+  array.map do |element|
+    element == array[1] ? element : element + "s"
+  end
+end
